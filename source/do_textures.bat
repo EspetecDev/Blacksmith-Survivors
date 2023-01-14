8bd3d761bbb8aec6b3d95deb1ff9a9084301403b
Espetec <manutheking1991@gmail.com>
2023-01-14 16:19:00 +0100

    delete resources folder and upload tim font

--- source/do_textures.bat
+++ source/do_textures.bat
@@ -1,3 +1,4 @@
 rem ..\tools\BMP2TIM\BMP2TIM.exe textures\Enemy_Blue.bmp textures\Enemy_Blue.tim 8  -org=640,0 -clut=0,480
 rem ..\tools\BMP2TIM\BMP2TIM.exe textures\Enemy_Blue4.bmp textures\Enemy_Blue.tim 4 -org=640,0 -clut=0,480
-..\tools\BMP2TIM\BMP2TIM.exe textures\Enemy_Red.bmp textures\Hero.tim 8 -org=640,0 -clut=0,480
+..\tools\BMP2TIM\BMP2TIM.exe textures\fnt.bmp textures\fnt.tim 4 -org=640,0 -clut=0,480
+..\tools\BMP2TIM\BMP2TIM.exe textures\Enemy_Red.bmp textures\Hero.tim 8 -org=640,16 -clut=0,480
