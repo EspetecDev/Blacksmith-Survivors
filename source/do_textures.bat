26c02727126f661dd112c32d8ec20f5cd8337357
James <jwdeharopwd@gmail.com>
2023-01-14 19:56:11 +0100

    - Gaby toma cariño

--- source/do_textures.bat
+++ source/do_textures.bat
@@ -5,4 +5,9 @@
 ..\tools\BMP2TIM\BMP2TIM.exe textures\MainCharacter_Attack.bmp textures\HeroAttack.tim 8 -org=320,128 -clut=512,32 -mpink
 ..\tools\BMP2TIM\BMP2TIM.exe textures\BlackSmith_title_screen_8bit.bmp textures\TitleScreen.tim 8 -org=320,240 -clut=512,64 -mpink
 
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE1.bmp textures\tile1.tim 4 -org=384,0 -clut=512,128
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE2.bmp textures\tile1.tim 4 -org=384,0 -clut=512,128
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE1.bmp textures\tile2.tim 4 -org=384,64 -clut=512,144
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE3.bmp textures\tile3.tim 4 -org=384,128 -clut=512,160
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE4.bmp textures\tile4.tim 4 -org=384,192 -clut=512,176
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE5.bmp textures\tile5.tim 4 -org=384,256 -clut=512,192
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE6.bmp textures\tile6.tim 4 -org=384,320 -clut=512,208
