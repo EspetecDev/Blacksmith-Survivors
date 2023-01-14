e28bc3bb3f16177671430e3eed98f7938bb8448c
Gabi <gabristc23@gmail.com>
2023-01-14 11:24:11 +0100

    Uploading scene.

--- source/renderMeshes.h
+++ source/renderMeshes.h
@@ -7,13 +7,13 @@
 
 #define ASSET_SIZE 80
 #define ASSET_UV_SIZE 63
-//static SDC_VertexTextured QuadAssetVertices[] = {
-//    {{ -ASSET_SIZE / 2, -ASSET_SIZE / 2, 0, 0}, 0, ASSET_UV_SIZE},
-//    {{ -ASSET_SIZE / 2, ASSET_SIZE / 2, 0, 0}, 0, 0},
-//    {{ ASSET_SIZE / 2, ASSET_SIZE / 2, 0, 0}, ASSET_UV_SIZE, 0},
-//    {{ ASSET_SIZE / 2, -ASSET_SIZE / 2, 0, 0}, ASSET_UV_SIZE, ASSET_UV_SIZE}
-//};
-//static u_short QuadAssetIndices[] = { 0, 3, 1, 3, 2, 1 };
-//static SDC_Mesh3D QuadAssetMesh = { QuadAssetVertices, QuadAssetIndices, NULL, 6, 4, POLIGON_VERTEX_TEXTURED };
+static SDC_VertexTextured QuadAssetVertices[] = {
+    {{ -ASSET_SIZE / 2, -ASSET_SIZE / 2, 0, 0}, 0, ASSET_UV_SIZE},
+    {{ -ASSET_SIZE / 2, ASSET_SIZE / 2, 0, 0}, 0, 0},
+    {{ ASSET_SIZE / 2, ASSET_SIZE / 2, 0, 0}, ASSET_UV_SIZE, 0},
+    {{ ASSET_SIZE / 2, -ASSET_SIZE / 2, 0, 0}, ASSET_UV_SIZE, ASSET_UV_SIZE}
+};
+static u_short QuadAssetIndices[] = { 0, 3, 1, 3, 2, 1 };
+static SDC_Mesh3D QuadAssetMesh = { QuadAssetVertices, QuadAssetIndices, NULL, 6, 4, POLIGON_VERTEX_TEXTURED };
 
 #endif
