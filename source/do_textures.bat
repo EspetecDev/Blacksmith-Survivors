c2dad3d97e34be6aeeed176c0b2d5a507e69f810
Espetec <manutheking1991@gmail.com>
2023-01-15 01:40:42 +0100

    la raspa la invento, kubala con su balon

--- source/do_textures.bat
+++ source/do_textures.bat
@@ -6,8 +6,8 @@
 ..\tools\BMP2TIM\BMP2TIM.exe textures\BlackSmith_title_screen_8bit.bmp textures\titlescreen.tim 8 -org=320,256 -clut=512,64 -mpink
 
 ..\tools\BMP2TIM\BMP2TIM.exe textures\TILE1.bmp textures\tile1.tim 4 -org=384,0 -clut=512,128
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE2.bmp textures\tile2.tim 4 -org=384,64 -clut=512,144
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE3.bmp textures\tile3.tim 4 -org=384,128 -clut=512,160
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE4.bmp textures\tile4.tim 4 -org=384,192 -clut=512,176
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE5.bmp textures\tile5.tim 4 -org=384,256 -clut=512,192
-..\tools\BMP2TIM\BMP2TIM.exe textures\TILE6.bmp textures\tile6.tim 4 -org=384,320 -clut=512,208
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE2.bmp textures\tile2.tim 4 -org=384,32 -clut=512,144
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE3.bmp textures\tile3.tim 4 -org=384,64 -clut=512,160
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE4.bmp textures\tile4.tim 4 -org=384,96 -clut=512,176
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE5.bmp textures\tile5.tim 4 -org=384,128 -clut=512,192
+..\tools\BMP2TIM\BMP2TIM.exe textures\TILE6.bmp textures\tile6.tim 4 -org=384,160 -clut=512,208
--- source/scene/scene.c
+++ source/scene/scene.c
@@ -10,12 +10,39 @@ void SceneMap_Init(SceneMap* MapPtr)
     {
         for(int j = 0; j <  SceneTileHeight; j++)
         {
+            // int RandTile = 0
+            // if(rand() % 75 != 0)
+            int TileType = 0;
             Tile Data;
             Data.Coordinates.vx = i;
             Data.Coordinates.vy = j;
             Data.TopLeft.vx = i * SceneTileDim;
             Data.TopLeft.vy = j * SceneTileDim;
-            Data.RealImage = TimTile5;
+            switch (TileType % 5)
+            {
+                case 0:
+                Data.RealImage = TimTile1;
+                break;
+                case 1:
+                Data.RealImage = TimTile2;
+                break;
+                case 2:
+                Data.RealImage = TimTile3;
+                break;
+                case 3:
+                Data.RealImage = TimTile4;
+                break;
+                case 4:
+                Data.RealImage = TimTile5;
+                break;
+                case 5:
+                Data.RealImage = TimTile6;
+                break;
+            
+            default:
+                break;
+            }
+            Data.RealImage = TimTile2;
             MapPtr->MapTiles[i][j] = Data;
         }
     }
@@ -26,20 +53,32 @@ void SceneMap_Draw(SceneMap * MapPtr, VECTOR * CameraPosition)
     const DVECTOR UV = {SceneTileDim, SceneTileDim};
     const CVECTOR Color = {255, 255, 255, 255};
 
-    for(int i = 0; i <  SceneTileWidth; i++)
-    {
-        for(int j = 0; j <  SceneTileHeight; j++)
-        {
+    // for(int i = 0; i <  SceneTileWidth; i++)
+    // {
+    //     for(int j = 0; j <  SceneTileHeight; j++)
+    //     {
 
-            Tile * Data = &MapPtr->MapTiles[i][j];
-            int TopLeftX = Data->TopLeft.vx - CameraPosition->vx;
-            int TopLefty = Data->TopLeft.vy - CameraPosition->vy;
-            if(abs(TopLeftX) < RENDER_WIDTH && abs(TopLefty) < RENDER_HEIGHT)
-            {
-                dcRender_DrawSpriteRect(GEngineInstance.RenderPtr, &Data->RealImage, TopLeftX, TopLefty, SceneTileDim, SceneTileDim, &UV, &Color);
-            }
-        }
-    }
+    //         Tile * Data = &MapPtr->MapTiles[i][j];
+    //         int TopLeftX = Data->TopLeft.vx - CameraPosition->vx;
+    //         int TopLefty = Data->TopLeft.vy - CameraPosition->vy;
+    //         if(abs(TopLeftX) < RENDER_WIDTH && abs(TopLefty) < RENDER_HEIGHT)
+    //         {
+    //             dcRender_DrawSpriteRect(GEngineInstance.RenderPtr, &Data->RealImage, TopLeftX, TopLefty, SceneTileDim, SceneTileDim, &UV, &Color);
+    //         }
+    //     }
+    // }
+    Tile * Data = &MapPtr->MapTiles[0][0];
+    Tile * Data2 = &MapPtr->MapTiles[1][1];
+    Tile * Data3 = &MapPtr->MapTiles[2][2];
+    int TopLeftX = Data->TopLeft.vx - CameraPosition->vx;
+    int TopLefty = Data->TopLeft.vy - CameraPosition->vy;
+    dcRender_DrawSpriteRect(GEngineInstance.RenderPtr, &TimTile2, TopLeftX, TopLefty, SceneTileDim, SceneTileDim, &UV, &Color);
+    TopLeftX = Data2->TopLeft.vx - CameraPosition->vx;
+    TopLefty = Data2->TopLeft.vy - CameraPosition->vy;
+    dcRender_DrawSpriteRect(GEngineInstance.RenderPtr, &TimTile3, TopLeftX, TopLefty, SceneTileDim, SceneTileDim, &UV, &Color);
+    TopLeftX = Data3->TopLeft.vx - CameraPosition->vx;
+    TopLefty = Data3->TopLeft.vy - CameraPosition->vy;
+    dcRender_DrawSpriteRect(GEngineInstance.RenderPtr, &TimTile4, TopLeftX, TopLefty, SceneTileDim, SceneTileDim, &UV, &Color);
 }
 
 VECTOR GetRandomLocation(SceneMap * MapPtr)
